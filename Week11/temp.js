4a91b0d828fa6dcb894c827ef49ad6241e3d68b7
xddregan <dregan@gmail.com>
2019-04-11 08:54:30 -0700

    week11'

---
+++ Week11/temp.js
@@ -0,0 +1,8 @@
+
+		<script src="js/three.js"></script>
+
+		<script src="http://threejs.org/examples/js/WebGL.js"></script>
+		<script src="http://threejs.org/examples/js/libs/stats.min.js"></script>
+		<script src="http://threejs.org/examples/js/controls/OrbitControls.js"></script>
+		<script src="http://threejs.org/examples/js/libs/dat.gui.min.js"></script>	
+		<script src="http://threejs.org/examples/js/loaders/GLTFLoader.js"></script>
