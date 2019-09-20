bb567865cf1cdbf65dcc8c8c4ca0ac1338c449f9
xddregan <dregan@gmail.com>
2019-09-19 17:44:29 -0700

    2019f

--- Week11/temp.js
+++
@@ -1,8 +0,0 @@
-
-		<script src="js/three.js"></script>
-
-		<script src="http://threejs.org/examples/js/WebGL.js"></script>
-		<script src="http://threejs.org/examples/js/libs/stats.min.js"></script>
-		<script src="http://threejs.org/examples/js/controls/OrbitControls.js"></script>
-		<script src="http://threejs.org/examples/js/libs/dat.gui.min.js"></script>	
-		<script src="http://threejs.org/examples/js/loaders/GLTFLoader.js"></script>
--- readme.md
+++
@@ -1,5 +0,0 @@
-
-This is clone of github for class files
-https://github.com/xddregan/cogswell.git
-
-
